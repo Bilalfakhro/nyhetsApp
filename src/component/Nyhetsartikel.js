ff62d76e7a0d088f02efda256b67c52d169aa19e
Bilal <bilal.fakhro@hotmail.se>
2019-04-01 11:04:44 +0200

    Lagt till egna komponenter

---
+++ src/component/Nyhetsartikel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Nyhetsartikel() {
+    return (
+    <section>
+            <article>
+            <img src="https://source.unsplash.com/random/200x150/?cat"></img>
+            <h2> testing </h2>
+            <p>beskrivning av testing</p>
+            <a>läs mer...</a>
+        </article>
+    </section>);
+}
+
+export default Nyhetsartikel;
---
+++ src/component/Nyhetslista.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from '../App';
+import Nyhetsartikel from './Nyhetsartikel';
+
+function Nyhetslista() {
+    return (
+        <section>
+            <Nyhetsartikel />
+            <Nyhetsartikel />
+            <Nyhetsartikel />
+            <Nyhetsartikel />
+        </section>);
+}
+
+export default Nyhetslista;
